52518e827650c0578d8a455e7ecbe8bfc149cb23
Jan Bogdanski <jan.b@aol.pl>
2012-01-28 13:30:18 +0100

    Dodanie kilku regul dla 'pl'

--- details.php
+++ details.php
@@ -2,16 +2,18 @@
 
 class Module_Sample extends Module {
 
-	public $version = '2.0';
+	public $version = '1.0';
 
 	public function info()
 	{
 		return array(
 			'name' => array(
-				'en' => 'Sample'
+				'pl' => 'Przyklad',
+				'en' => 'Sample',
 			),
 			'description' => array(
-				'en' => 'This is a PyroCMS module sample.'
+				'pl' => 'Modul.',
+				'en' => 'This is a PyroCMS module sample.',
 			),
 			'frontend' => TRUE,
 			'backend' => TRUE,
@@ -34,56 +36,56 @@ class Module_Sample extends Module {
 
 	public function install()
 	{
-		$this->dbforge->drop_table('sample');
-		$this->db->delete('settings', array('module' => 'sample'));
-
-		$sample = array(
-                        'id' => array(
-									  'type' => 'INT',
-									  'constraint' => '11',
-									  'auto_increment' => TRUE
-									  ),
-						'name' => array(
-										'type' => 'VARCHAR',
-										'constraint' => '100'
-										),
-						'slug' => array(
-										'type' => 'VARCHAR',
-										'constraint' => '100'
-										)
-						);
-
-		$sample_setting = array(
-			'slug' => 'sample_setting',
-			'title' => 'Sample Setting',
-			'description' => 'A Yes or No option for the Sample module',
-			'`default`' => '1',
-			'`value`' => '1',
-			'type' => 'select',
-			'`options`' => '1=Yes|0=No',
-			'is_required' => 1,
-			'is_gui' => 1,
-			'module' => 'sample'
-		);
-
-		$this->dbforge->add_field($sample);
-		$this->dbforge->add_key('id', TRUE);
-
-		if($this->dbforge->create_table('sample') AND
-		   $this->db->insert('settings', $sample_setting) AND
-		   is_dir($this->upload_path.'sample') OR @mkdir($this->upload_path.'sample',0777,TRUE))
-		{
-			return TRUE;
-		}
+//		$this->dbforge->drop_table('sample');
+//		$this->db->delete('settings', array('module' => 'sample'));
+//
+//		$sample = array(
+//                        'id' => array(
+//									  'type' => 'INT',
+//									  'constraint' => '11',
+//									  'auto_increment' => TRUE
+//									  ),
+//						'name' => array(
+//										'type' => 'VARCHAR',
+//										'constraint' => '100'
+//										),
+//						'slug' => array(
+//										'type' => 'VARCHAR',
+//										'constraint' => '100'
+//										)
+//						);
+//
+//		$sample_setting = array(
+//			'slug' => 'sample_setting',
+//			'title' => 'Sample Setting',
+//			'description' => 'A Yes or No option for the Sample module',
+//			'`default`' => '1',
+//			'`value`' => '1',
+//			'type' => 'select',
+//			'`options`' => '1=Yes|0=No',
+//			'is_required' => 1,
+//			'is_gui' => 1,
+//			'module' => 'sample'
+//		);
+//
+//		$this->dbforge->add_field($sample);
+//		$this->dbforge->add_key('id', TRUE);
+//
+//		if($this->dbforge->create_table('sample') AND
+//		   $this->db->insert('settings', $sample_setting) AND
+//		   is_dir($this->upload_path.'sample') OR @mkdir($this->upload_path.'sample',0777,TRUE))
+//		{
+//			return TRUE;
+//		}
 	}
 
 	public function uninstall()
 	{
-		$this->dbforge->drop_table('sample');
-		$this->db->delete('settings', array('module' => 'sample'));
-		{
-			return TRUE;
-		}
+//		$this->dbforge->drop_table('sample');
+//		$this->db->delete('settings', array('module' => 'sample'));
+//		{
+//			return TRUE;
+//		}
 	}
 
 
